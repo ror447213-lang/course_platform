888dea881c6afbefc77cfc634269e59db0a902b4
ror447213-lang <ror447213@gmail.com>
2026-04-17 22:30:58 +0530

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -19,8 +19,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+def hash_password(password):
+    return pwd_context.hash(password[:72])
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
